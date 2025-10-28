927c0c98e0c9fc8d509ccdb3be31e842ba84dc88
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-28 14:42:09 +0700

    menambahkan struktur tabel komik dengan kolom id, judul, penulis, dan deskripsi.

--- models/komik.js
+++ models/komik.js
@@ -1,3 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-    
+    const komik = sequelize.define('komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        penulis: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+
+    });
+    return komik;
 }
