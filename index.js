44f1ace6c32320b81cdb6789cbefe803dd780ce5
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-28 14:46:08 +0700

    menambahkan middleware express.json() dan express.urlencoded() untuk menangani request body.

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
