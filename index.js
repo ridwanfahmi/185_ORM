07943c5d6b7f31a5894e3c492d0cf144bcd6a5a2
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-28 14:46:57 +0700

    menambahkan log untuk memastikan server berjalan pada port tertentu.

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+app.listen(PORT, async() => {
+    console.log(`Server is running on port ${PORT}`);
+});
